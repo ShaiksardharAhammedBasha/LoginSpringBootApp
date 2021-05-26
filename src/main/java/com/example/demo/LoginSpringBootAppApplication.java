4f5e5946a0b73124154ba48aece621fcb6083066
ShaiksardharAhammedBasha <smabasha222@gmail.com>
2021-05-26 17:44:45 +0530

    pushing entiere project to repo

---
+++ src/main/java/com/example/demo/LoginSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringBootAppApplication.class, args);
+	}
+
+}
